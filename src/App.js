51bf26c7245f97320f41981a1955f044cb4cbae6
Andrew Hayes <andrew@getslingshot.com>
2019-03-12 08:32:08 -0600

    repo for practicing hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+ 
+const decrement = () => {
+  setCount(count - 1)
 }
 
-export default App;
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Add me
+      </button>
+      <button onClick={ decrement }>
+        Minus Me
+      </button>
+    </div>
+  );
+}
+
+export default Example;
